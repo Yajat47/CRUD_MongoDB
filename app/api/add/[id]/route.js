9afccef4a2ca8d1c37d4dd26f0f1eaf99d96e391
Yajat47 <yajatsmurthy@gmail.com>
2023-11-14 11:54:33 +0530

    Fixed Bugs

--- app/api/add/[id]/route.js
+++ app/api/add/[id]/route.js
@@ -1,25 +1,25 @@
-import connectMongoDB from "@/libs/mongodb";
-import Topic from "@/models/batch";
-import { NextResponse } from "next/server";
+// import connectMongoDB from "@/libs/mongodb";
+// import Topic from "@/models/batch";
+// import { NextResponse } from "next/server";
 
-export async function PUT(request, { params }) {
-  const { id } = params;
-  const { newTitle: title, newDescription: description } = await request.json();
-  await connectMongoDB();
-  await Topic.findByIdAndUpdate(id, { title, description });
-  return NextResponse.json({ message: "Topic updated" }, { status: 200 });
-}
-
-// export async function GET(request, { params }) {
+// export async function PUT(request, { params }) {
 //   const { id } = params;
+//   const { newTitle: title, newDescription: description } = await request.json();
 //   await connectMongoDB();
-//   const topic = await Topic.findOne({ _id: id });
-//   return NextResponse.json({ topic }, { status: 200 });
+//   await Topic.findByIdAndUpdate(id, { title, description });
+//   return NextResponse.json({ message: "Topic updated" }, { status: 200 });
 // }
 
-export async function GET(request) {
+// // export async function GET(request, { params }) {
+// //   const { id } = params;
+// //   await connectMongoDB();
+// //   const topic = await Topic.findOne({ _id: id });
+// //   return NextResponse.json({ topic }, { status: 200 });
+// // }
+
+// export async function GET(request) {
   
-  await connectMongoDB();
-  const info = await Batch.findOne({ _id: id });
-  return NextResponse.json({ topic }, { status: 200 });
-}
+//   await connectMongoDB();
+//   const info = await Batch.findOne({ _id: id });
+//   return NextResponse.json({ topic }, { status: 200 });
+// }
