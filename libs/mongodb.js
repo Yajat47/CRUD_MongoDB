ffbbdbe22d2a4fa61dde840072ec8a688b69455f
Yajat47 <yajatsmurthy@gmail.com>
2023-11-14 17:50:02 +0530

    Exp1

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODBURI);
+    await mongoose.connect("mongodb+srv://dev:Abcd1234\$@serverlessinstance0.pmbhjm0.mongodb.net/Tracker?retryWrites=true&w=majority");
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
